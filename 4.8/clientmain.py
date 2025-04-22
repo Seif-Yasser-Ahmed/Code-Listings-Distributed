367e0ea457683040620d2afb5ce7c9066a0ed5aa
Seif Yasser <126684081+Seif-Yasser-Ahmed@users.noreply.github.com>
2025-04-22 19:35:32 +0200

    Update clientmain.py

--- 4.8/clientmain.py
+++ 4.8/clientmain.py
@@ -1,15 +1,20 @@
-import rpc1 
+import redis
+import rpc1
+from constRPC import *
+import json
+
+def client(channelID, clientID, serverID):
+    redis_client = redis.StrictRedis(host='192.168.100.4', port=6379, db=0)
+    s1 = rpc1.DBList([1, 2])
+    message = {'operation': 'append', 'data': 'c', 'list': s1.value}
+
+
+    redis_client.publish('append_channel', json.dumps(message))
+
+    print("Value s1:", s1.value)
+
+
 channelID = 1
-serverID  = 10
-clientID  = 20
-
-def main():
-    c = rpc1.Client(channelID, clientID, serverID) # Create client stub
-    s1 = rpc1.DBList((1,2))      # Create a local list
-    s2 = c.append('c',s1)        # Pass local list to stub and wait for result
-    print("Value s1:", s1.value) # This is what client started with
-    print("Value s2:", s2.value) # This is what the server did after call to append
-    c.stop()
-
-if _name_ == "_main_":
-    main()
+clientID = 20
+serverID = 10
+client(channelID, clientID, serverID)
