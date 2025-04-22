e7dc63d3c1ee0036f87b5b522844dba0b405831a
Seif Yasser <126684081+Seif-Yasser-Ahmed@users.noreply.github.com>
2025-04-22 19:16:50 +0200

    Update servermain.py

--- 4.8/servermain.py
+++ 4.8/servermain.py
@@ -1,12 +1,30 @@
+import redis
 import rpc1
+import json
+from constRPC import *
 
-# Fix the identities of channel, server, and client
+def server(channelID, serverID):
+    print("Server is running...")
+    redis_client = redis.StrictRedis(host='192.168.100.78', port=6379, db=0)
+    pubsub = redis_client.pubsub()
+    pubsub.subscribe('append_channel')
+
+    for message in pubsub.listen():
+        if message['type'] == 'message':
+            try:
+
+                message_data = json.loads(message['data'].decode('utf-8')) 
+                operation = message_data.get('operation')
+
+                if operation == 'append':
+
+                    db_list = rpc1.DBList(message_data['list'])
+                    result = db_list.append(message_data['data'])
+                    print(f"Server received append request. Updated list: {result.value}")
+            except Exception as e:
+                print(f"Error processing message: {e}")
+
+
 channelID = 1
-serverID  = 10
-
-def main():
-    s = rpc1.Server(channelID, serverID)
-    s.run()
-
-if _name_ == "_main_":
-    main()
+serverID = 10
+server(channelID, serverID)
